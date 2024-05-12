eb0bdcc8616d8e9ff567be0e82d75e08ee95d4ec
ariffuzu001@gmail.com <ariffuzu001@gmail.com>
2024-05-13 01:32:05 +0530

    Add project files.

---
+++ WebAPIK8s/SeedData.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebAPIK8s
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new DbContextClass(serviceProvider.GetRequiredService<DbContextOptions<DbContextClass>>()))
+            {
+                if (context.Customers.Any()) { return; }
+                context.Customers.AddRange(new CustomerDetails { CustomerId = 1, CustomerName = "Arif", CustomerAddress = "Delhi", CustomerNumber = 7454947490, CustomerPinCode = 244001 });
+                context.SaveChanges();
+            }
+        }
+    }
+}
